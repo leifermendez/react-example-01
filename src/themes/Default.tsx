4a2afaa05968de5ebbd69b16540bb34aaefa9aaf
Leifer Mendez <leifer33@gmail.com>
2022-12-17 18:53:03 +0100

    feat: :zap: Iniciando

---
+++ src/themes/Default.tsx
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+const Theme = createGlobalStyle`
+    
+    body{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+`;
+
+export default Theme;
